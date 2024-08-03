ca044f605f8544559a7f1e7f341251bfc9cf6aab
rahulyadav1708 <141802799+rahulyadav1708@users.noreply.github.com>
2024-08-03 13:35:02 +0530

    Add files via upload

---
+++ set_2 databaseQ4.sql
@@ -0,0 +1,2 @@
+select COUNT(FIRSTNAME) as count from tablename where Class='7w/ict' And Gender='M' 
+   // In this  query it will fetch the count of student firstname  whose class is 7w/ict and gender is male
